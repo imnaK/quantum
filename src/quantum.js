54173518a79008e2929c97b90ad5f80d2f0844bf
Traube1000101 <girlscoutcookies420@gmx.net>
2023-12-17 17:02:07 +0100

    added msg receive handler & decoding test (console.log)

--- src/quantum.js
+++ src/quantum.js
@@ -2,6 +2,7 @@ import branca from "branca";
 import secret from "./.secret.json";
 
 const XChaCha20_Poly1305 = new branca(secret.key);
+const { Patcher, Webpack } = BdApi;
 
 export default class Quantum {
   commandPrefix = "q:";
@@ -9,8 +10,8 @@ export default class Quantum {
   constructor(meta) {}
 
   start() {
-    let _sendMessage = BdApi.Webpack.getModule(
-      BdApi.Webpack.Filters.byProps("_sendMessage")
+    let _sendMessage = Webpack.getModule(
+      Webpack.Filters.byProps("_sendMessage")
     );
     BdApi.Patcher.before(
       "encryptMessage",
@@ -20,12 +21,40 @@ export default class Quantum {
         if (args[1].content.startsWith(this.commandPrefix))
           args[1].content =
             this.commandPrefix +
-            XChaCha20_Poly1305.encode(args[1].content.substring(this.commandPrefix.length));
+            XChaCha20_Poly1305.encode(
+              args[1].content.substring(this.commandPrefix.length)
+            );
       }
+    );
+
+    let dispatchModule = BdApi.findModuleByProps("dispatch", "subscribe");
+    Patcher.after(
+      "receiveMessage",
+      dispatchModule,
+      "dispatch",
+      this.handleMessage.bind(this)
     );
   }
 
   stop() {
-    BdApi.Patcher.unpatchAll("encryptMessage");
+    Patcher.unpatchAll("encryptMessage");
+    Patcher.unpatchAll("receiveMessage");
+  }
+
+  handleMessage(_, args) {
+    try {
+      if (args[0].type !== "MESSAGE_CREATE") return;
+
+      const decoder = new TextDecoder();
+
+      let { message } = args[0];
+      if (message.content.startsWith(this.commandPrefix)) {
+        console.log("Received quantum message, starting decryption...")
+        let decryptedUint8Array = XChaCha20_Poly1305.decode(message.content.substring(this.commandPrefix.length))
+        console.log("Decrypted Message: " + decoder.decode(decryptedUint8Array));
+      }
+    } catch (e) {
+      console.log(error(`${e}`));
+    }
   }
 }
