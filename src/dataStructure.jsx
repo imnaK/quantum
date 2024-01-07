a8526c43413be9936275cb76fe0f0afd7efad2f8
Traube1000101 <girlscoutcookies420@gmx.net>
2024-01-07 23:42:09 +0100

    Added save/load functions to dataStructure

--- src/dataStructure.jsx
+++ src/dataStructure.jsx
@@ -3,51 +3,87 @@ import InputField from "./inputField";
 
 import * as fs from "fs";
 import branca from "branca";
-import crypto from "crypto";
-import { eventNames } from "process";
+import * as scryptJs from "scrypt-js";
 
 const dataDirectory = __dirname + "/../quantum/";
-const userId = BdApi.Webpack.getModule(
-  BdApi.Webpack.Filters.byKeys("getCurrentUser")
-).getCurrentUser().id;
-
-let dataObject = {};
 
 export default class dataStructure {
-  constructor() {
-    //const XChaCha20_Poly1305 = new branca();
-    //const key1 = scryptSync("password", userId, 64);
+  #hashedPassword = null;
+  #dataObject = {};
+  userId = null;
 
-    showPasswordModal((value) => {
-      console.log("Password confirmed - Value: " + value);
+  constructor(userId) {
+    // Probably could be refined using Comma Operator
+    this.userId =
+      userId == null
+        ? BdApi.Webpack.getModule(BdApi.Webpack.Filters.byKeys("getCurrentUser")).getCurrentUser().id
+        : userId;
+
+    showPasswordModal(async (password) => {
+      Quantum.log("Hashing password...");
+
+      const encoder = new TextEncoder();
+      let passwordUint8Array = encoder.encode(password);
+      let userIdUint8Array = encoder.encode(this.userId);
+      const N = 1024,
+        r = 8,
+        p = 1,
+        dkLen = 32;
+      this.#hashedPassword = await scryptJs.scrypt(passwordUint8Array, userIdUint8Array, N, r, p, dkLen, (progress) => {
+        console.log(Math.trunc(100 * progress) + "%");
+      });
+      Quantum.log([...this.#hashedPassword].map((x) => x.toString(16).padStart(2, "0")).join(""));
     });
   }
 
+  decrypt(encryptedData) {
+    const XChaCha20_Poly1305 = new branca(this.#hashedPassword);
+    return XChaCha20_Poly1305.decode(encryptedData);
+  }
+
+  encrypt(jsonData) {
+    const XChaCha20_Poly1305 = new branca(this.#hashedPassword);
+    return XChaCha20_Poly1305.encode(jsonData);
+  }
+
+  load() {
+    try {
+      let encryptedData = fs.readFileSync(dataDirectory + "data_" + this.userId);
+      let decryptedJsonData = this.decrypt(encryptedData);
+      let dataObject = JSON.parse(decryptedJsonData);
+
+      console.log("Loaded & decrypted data object: ", dataObject);
+    } catch (error) {
+      Quantum.error(error);
+      return -1;
+    }
+  }
+
   save() {
-    let dataJSON = JSON.stringify(dataObject);
+    let jsonData = JSON.stringify(this.#dataObject);
+
+    let encryptedData = this.encrypt(jsonData);
+    Quantum.log("Encrypted data: ", encryptedData);
 
     if (!fs.existsSync(dataDirectory)) {
       fs.mkdirSync(dataDirectory, { recursive: true });
     }
-    fs.writeFile(
-      dataDirectory + "data_" + userId,
-      Buffer.from(dataObject),
-      function (err) {
-        if (err) throw err;
-        console.log(Quantum.logPrefix + "Saved data");
+    fs.writeFile(dataDirectory + "data_" + this.userId, Buffer.from(encryptedData), function (error) {
+      if (error) {
+        Quantum.error(error);
+        return -1;
       }
-    );
+      Quantum.log("Saved data to file");
+    });
   }
 
   get(key) {
-    return dataObject[key];
+    return this.#dataObject[key];
   }
 
   set(key, value) {
-    dataObject[key] = value;
+    this.#dataObject[key] = value;
   }
-
-  encrypt() {}
 }
 
 function showPasswordModal(callback) {
@@ -55,17 +91,17 @@ function showPasswordModal(callback) {
 
   const handleConfirm = () => {
     callback(inputRef.current.getValue());
-    BdApi.findModuleByProps("closeModal").closeModal(modalId);
-  }
+    BdApi.Webpack.getModule(BdApi.Webpack.Filters.byKeys("closeModal")).closeModal(modalId);
+  };
 
   let modalId = BdApi.UI.showConfirmationModal(
     "Quantum Password",
-    <InputField ref={inputRef} handleConfirm={handleConfirm} />,
+    <InputField ref={inputRef} handleConfirm={handleConfirm} type="password" />,
     {
       confirmText: "Enter",
       cancelText: "Nevermind",
       onConfirm: handleConfirm,
-      onCancel: () => console.log("Pressed 'Nevermind'"),
+      onCancel: () => Quantum.log("Pressed 'Nevermind'"),
     }
   );
 }
