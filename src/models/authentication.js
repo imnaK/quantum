e5628a8166b37526fa8bc969ab3dfa46340a8f21
Traube1000101 <girlscoutcookies420@gmx.net>
2024-01-20 11:48:24 +0100

    renamed naclUtil and corrected Qunatum.log

--- src/models/authentication.js
+++ src/models/authentication.js
@@ -1,10 +1,11 @@
 import * as nacl from "tweetnacl";
 import * as naclUtil from "tweetnacl-util";
 import Quantum from "@quantum";
+import * as logger from "@utils/logger";
 
 export default class authentication {
   constructor() {
-    Quantum.log(Quantum.data.getKey("keyPair"));
+    logger.log(Quantum.data.getKey("keyPair"));
     //Quantum.log("Quantum.data.getKey(\"keyPair\")", Quantum.data.getKey("keyPair") || nacl.box.keyPair());
     // if (this.keyPair = Quantum.data.getKey("keyPair") == undefined) {
     //   this.keyPair = nacl.box.keyPair();
@@ -21,8 +22,8 @@ export default class authentication {
 
   encrypt(receiverPublicKey, msgParams) {
     const ephemeralKeyPair = nacl.box.keyPair();
-    const pubKeyUInt8Array = util.decodeBase64(receiverPublicKey);
-    const msgParamsUInt8Array = util.decodeUTF8(msgParams);
+    const pubKeyUInt8Array = naclUtil.decodeBase64(receiverPublicKey);
+    const msgParamsUInt8Array = naclUtil.decodeUTF8(msgParams);
     const nonce = nacl.randomBytes(nacl.box.nonceLength);
     const encryptedMessage = nacl.box(
       msgParamsUInt8Array,
@@ -31,24 +32,24 @@ export default class authentication {
       ephemeralKeyPair.secretKey
     );
     return {
-      ciphertext: util.encodeBase64(encryptedMessage),
-      ephemPubKey: util.encodeBase64(ephemeralKeyPair.publicKey),
-      nonce: util.encodeBase64(nonce),
+      ciphertext: naclUtil.encodeBase64(encryptedMessage),
+      ephemPubKey: naclUtil.encodeBase64(ephemeralKeyPair.publicKey),
+      nonce: naclUtil.encodeBase64(nonce),
     };
   }
 
   decrypt(receiverSecretKey, encryptedData) {
-    const receiverSecretKeyUint8Array = util.decodeBase64(receiverSecretKey);
-    const nonce = util.decodeBase64(encryptedData.nonce);
-    const ciphertext = util.decodeBase64(encryptedData.ciphertext);
-    const ephemPubKey = util.decodeBase64(encryptedData.ephemPubKey);
+    const receiverSecretKeyUint8Array = naclUtil.decodeBase64(receiverSecretKey);
+    const nonce = naclUtil.decodeBase64(encryptedData.nonce);
+    const ciphertext = naclUtil.decodeBase64(encryptedData.ciphertext);
+    const ephemPubKey = naclUtil.decodeBase64(encryptedData.ephemPubKey);
     const decryptedMessage = nacl.box.open(
       ciphertext,
       nonce,
       ephemPubKey,
       receiverSecretKeyUint8Array
     );
-    return util.encodeUTF8(decryptedMessage);
+    return naclUtil.encodeUTF8(decryptedMessage);
   }
 
   sendChatMessage(content, channelId) {
