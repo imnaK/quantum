3854ca863ab5314eac39064efb0be5f0c16ea7f3
imnaK <mc.yama1000@gmail.com>
2024-01-21 01:29:24 +0100

    refactor: i18n.js code cleanup

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,24 +1,25 @@
-import translations from '/src/i18n/translations';
+import translations from "/src/i18n/translations";
 
 let language = null;
 let observer = null;
 
-const getCurrentLanguage = () => document.documentElement.lang.split('-')[0];
+// Get the current language from the document
+const getCurrentLanguage = () => document.documentElement.lang.split("-")[0];
 
 function init() {
   setLanguage(getCurrentLanguage());
 
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (mutation.attributeName === 'lang') {
-        setLanguage(getCurrentLanguage());
-      }
-    });
+  // Observe changes to the 'lang' attribute of the document
+  observer = new MutationObserver((mutations) => {
+    if (mutations.some((mutation) => mutation.attributeName === "lang")) {
+      setLanguage(getCurrentLanguage());
+    }
   });
 
   observer.observe(document.documentElement, { attributes: true });
 }
 
+// Disconnect the observer when it's no longer needed
 function cleanup() {
   if (observer) {
     observer.disconnect();
@@ -26,16 +27,13 @@ function cleanup() {
   }
 }
 
-function setLanguage(newLanguage) {
+// Set the current language, default to 'en' if not found in translations
+function setLanguage(lang) {
   if (newLanguage === language) return;
-
-  if (translations[newLanguage]) {
-    language = newLanguage;
-  } else {
-    language = 'en';
-  }
+  language = translations[lang] ? lang : "en";
 }
 
+// Return the translation for the key if it exists, otherwise return the key itself
 function translate(key) {
   return translations[language][key] || key;
 }
