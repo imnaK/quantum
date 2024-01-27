e828bdcd3172b86d54744b80be62e0cfdb24b0d2
imnaK <mc.yama1000@gmail.com>
2024-01-27 14:33:58 +0100

    feat: constant for quantum save path

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,15 @@
 const QUANTUM_PREFIX = "q:";
 const QUANTUM_CLASS = "quantum";
 const QUANTUM_ENCRYPTION_FILE_NAME = "quantum-keys.enc";
+const QUANTUM_ENCRYPTION_DIRECTORY_PATH = path.resolve(
+  __dirname,
+  "..",
+  "quantum"
+);
 
-export { QUANTUM_PREFIX, QUANTUM_CLASS, QUANTUM_ENCRYPTION_FILE_NAME };
+export {
+  QUANTUM_PREFIX,
+  QUANTUM_CLASS,
+  QUANTUM_ENCRYPTION_FILE_NAME,
+  QUANTUM_ENCRYPTION_DIRECTORY_PATH,
+};
